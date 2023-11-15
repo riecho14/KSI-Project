436775697e43acd3da0b5bef5d33bc7b67d37974
riecho14 <riecho14@outlook.com>
2023-11-15 23:04:01 +0700

    teamsar

--- app/src/main/java/com/sil/ecohero/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/sil/ecohero/ui/notifications/NotificationsFragment.kt
@@ -18,20 +18,11 @@ class NotificationsFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
